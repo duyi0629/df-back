a2077709292324ae99979313140f4e600670ce4c
df <jmmm20152019@163.com>
2025-04-09 18:02:29 +0800

    样式更新

--- src/views/article/list/index.tsx
+++ src/views/article/list/index.tsx
@@ -1,26 +1,31 @@
-import { Table, TableBody, TableCell, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import React, { memo, useEffect, useState } from "react";
 import { getArticleList } from "@/service/modules/article";
 
 interface ArticlePorps {
-    _id: string,
-    title: string,
-    content: string,
-    createdAt: string,
-
+  _id: string;
+  title: string;
+  content: string;
+  createdAt: string;
 }
 
 const ArticleList = memo(() => {
-    const [articleList, setArticleList] = useState<ArticlePorps[]>([])
-    useEffect(() => {
-        const getArticleListData = async () => {
-            const res = await getArticleList()
-            if(res.code === 200) {  
-                setArticleList(res.data)
-            }
-        }
-        getArticleListData();
-    }, [])
+  const [articleList, setArticleList] = useState<ArticlePorps[]>([]);
+  useEffect(() => {
+    const getArticleListData = async () => {
+      const res = await getArticleList();
+      if (res.code === 200) {
+        setArticleList(res.data);
+      }
+    };
+    getArticleListData();
+  }, []);
   return (
     <div className="py-6 px-8">
       <Table>
@@ -28,25 +33,22 @@ const ArticleList = memo(() => {
           <TableRow>
             <TableCell>序号</TableCell>
             <TableCell>文章标题</TableCell>
-            <TableCell>文章内容</TableCell>
+            <TableCell className="w-[500px]">文章内容</TableCell>
             <TableCell>创建时间</TableCell>
           </TableRow>
         </TableHeader>
 
         <TableBody>
-            {
-                articleList.map((article, aIndex) => {
-                    return (
-                    <TableRow key={article._id}>
-                                <TableCell>{aIndex + 1}</TableCell>
-                                <TableCell>{article.title}</TableCell>
-                                <TableCell>{article.content}</TableCell>
-                                <TableCell>{article.createdAt}</TableCell>
-                            </TableRow>
-
-                    )
-                })
-            }
+          {articleList.map((article, aIndex) => {
+            return (
+              <TableRow key={article._id}>
+                <TableCell>{aIndex + 1}</TableCell>
+                <TableCell>{article.title}</TableCell>
+                <TableCell className=" w-[500px] overflow-hidden text-ellipsis whitespace-nowrap inline-block">{article.content}</TableCell>
+                <TableCell>{article.createdAt}</TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
     </div>
